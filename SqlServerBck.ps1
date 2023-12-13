5fe007acaa3997784c1b465142f41a9657e8b409
LoneMarvel <tinost@gmail.com>
2023-12-13 15:16:02 +0200

    Add files via upload
    Add SqlServer Backup Script v1

---
+++ SqlServerBck.ps1
@@ -0,0 +1,19 @@
+﻿# Declare variables needed for the Script
+$instanceName = 'DESKTOP-RTCARLP\SEMANTICINST' # The Name that is used in SqlManagement
+$ignoreDb = ('master','model','msdb','tempdb') # Array contains the DataBases that you don't want to backed up
+$dateExt = Get-Date -Format 'ddMMyyyyHHmmss' # This variable is used as name extension (not file extension) for unique reason
+$bckDir = 'C:\tmp\SqlBackup\' # Temporary local directory
+$netDir = 'Y:\BackupSql\' # Network directory finally destination
+# Script is starting
+$dbList = Get-SqlDatabase -ServerInstance $instanceName
+
+foreach($dbTable in $dbList){    
+    $dbName = $dbTable.Name    
+    if ( !$ignoreDb.Contains($dbName) ){
+        $fileName = $dbName+'-'+$dateExt+'.bak'
+        Backup-SqlDatabase -ServerInstance $instanceName -Database $dbName -BackupFile $bckDir$fileName
+        Copy-Item $bckDir$fileName -Destination $netDir
+        Remove-Item $bckDir$fileName
+    }
+}
+
